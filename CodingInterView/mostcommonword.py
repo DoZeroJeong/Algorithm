c9430bb23c9dfd926bea4fea1c6e63d113d4afa9
JEONG <dogug2865@gmail.com>
2020-12-07 22:50:39 +0900

    feat: Algorithm Example

--- CodingInterView/mostcommonword.py
+++ CodingInterView/mostcommonword.py
@@ -17,7 +17,7 @@ def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
     # 정규식 \w 표현은 문자를 뜻한다. 앞에 ^은 not을 의미한다.
     words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
     # [word for word in re.sub(r'[^\w]', ' ', paragraph)]는 리스트형태로 저장된다.
-    
+
     counts = collections.Counter(words)
     return counts.most_common(1)[0][0]
 
@@ -25,5 +25,3 @@ def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
 print(mostCommonWord(self=None,
                      paragraph="Bob hit al ball, the hit BALL flew far after it was hit.",
                      banned=["hit"]))
-
-
